9eb614fd26af8441f84b965ad54d0e2c07470ff3
xdj701 <dajunxu2@gmail.com>
2022-06-30 22:22:03 -0400

    Update and rename ocr.py to scan_file.py
    enable the reading of machine-generated pdf files

--- scan_file.py
+++ scan_file.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import pytesseract
+import pdfplumber
 import re
 from pdf2image import convert_from_path
 from nltk.corpus import words
@@ -19,24 +20,10 @@ def erode_and_dilate_image(img):
 
     return dilated_img
 
-# return a string
-def scan_pdf(pdf_path):
-    pdf = convert_from_path(pdf_path)
-
-    # return a list of text in each page of the input doc
-    pages = [pytesseract.image_to_string(erode_and_dilate_image(page), lang="eng", config="--psm 3") for page in pdf]
-    for i in range(len(pages)):
-        page = pages[i]
-        lines = page.strip("\n").split("\n\n")
-        # check if the last line contains page number, file path, or doc name
-        if not any([token in words.words() for token in lines[-1].split(" ")]):
-            lines.pop()
-            pages[i] = "\n\n".join(lines)
-
-    text = "\n".join(pages)
+def fix_ocr_errors(raw_text):
     
     # Gj) -> (j)
-    text = re.sub("G\w\)", lambda o: "(" + o.group()[1:], text)
+    text = re.sub("G\w\)", lambda o: "(" + o.group()[1:], raw_text)
     # {a) -> (a)
     text = re.sub("\{\w\)", lambda o: "(" + o.group()[1:], text)
     # 1, -> 1., 3,3.1 -> 3.3.1 This may be too hacky
@@ -69,6 +56,42 @@ def scan_pdf(pdf_path):
         ).replace("ARTICLE hit", "ARTICLE III"
         ).replace("ARTICLE [V", "ARTICLE IV"
         ).replace("ARTICLE VH", "ARTICLE VII"
+        ).replace("ARTICLE V1", "ARTICLE VI"
+        ).replace("ARTICLE ¥", "ARTICLE V"
         ).replace("(ili)", "(iii)"
         ).replace("shal]", "shall"
         ).replace("&)", "(x)")
+
+def try_remove_footer(lines):
+    if ".doc" in lines or all([token not in words.words() for token in lines[-1].split(" ")]):
+        return "\n\n".join(lines[:-1])
+    return "\n\n".join(lines)
+
+# return a string  
+def scan_pdf(file_path):
+
+    with pdfplumber.open(file_path) as pdf:
+        page_texts = [page.extract_text(layout=True).strip() for page in pdf.pages]
+        # the pdf is machine-generated
+        if all(page_texts):
+            clean_texts = []
+            for page_text in page_texts:
+                raw_page_text = re.sub("[\n]{2,}", "\n\n", page_text)
+                lines = [re.sub("[\s\n]+", " ", line).strip() for line in raw_page_text.split("\n\n")]
+                clean_texts.append(try_remove_footer(lines))
+            
+            full_text = "\n\n".join(clean_texts)
+            full_text = re.sub("\( ?(\w+) ?\)", lambda o: "(" + o.groups()[0] + ")", full_text)
+            return full_text
+        # the pdf is a scanned copy, try OCR
+        else:
+            page_imgs = convert_from_path(file_path)
+
+            # return a list of text in each page of the input doc
+            page_texts = [pytesseract.image_to_string(erode_and_dilate_image(page), lang="eng", config="--psm 3") for page in page_imgs]
+            raw_texts = []
+            for page_text in page_texts:
+                lines = page_text.strip("\n").split("\n\n")
+                raw_texts.append(try_remove_footer(lines))   
+            full_text = "\n\n".join(raw_texts)
+            return fix_ocr_errors(full_text)
